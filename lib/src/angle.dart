25175d676a0d22ede5f1369275f89e9aae88dbf4
Mahdi <mkfard@outlook.com>
2024-02-06 22:29:49 +0330

    const condtructor.

--- lib/src/angle.dart
+++ lib/src/angle.dart
@@ -2,13 +2,13 @@ part of '../latlng.dart';
 
 /// Represents an angle.
 class Angle {
-  Angle._(this.degrees, this.radians);
+  const Angle._(this.degrees, this.radians);
 
   /// From degrees.
-  Angle.degree(double value) : this._(value, value * _deg2rad);
+  const Angle.degree(double value) : this._(value, value * _deg2rad);
 
   /// From degrees.
-  Angle.radian(double value) : this._(value * _rad2deg, value);
+  const Angle.radian(double value) : this._(value * _rad2deg, value);
 
   /// In degrees.
   final double degrees;
