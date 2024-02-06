e22a10cc2ec119e5ba99830cf4198b33b95a5483
Mahdi <mkfard@outlook.com>
2024-02-06 13:52:15 +0330

    Add LatLngAlt class.

--- lib/src/latlng.dart
+++ lib/src/latlng.dart
@@ -19,3 +19,31 @@ class LatLng {
     return LatLng(lat, lng);
   }
 }
+
+/// Coordinates in Degrees.
+class LatLngAlt {
+  /// Default Constructor.
+  const LatLngAlt(this.latitude, this.longitude, this.altitude);
+
+  /// Latitude, Y Axis.
+  final double latitude;
+
+  /// Longitude, X Axis.
+  final double longitude;
+
+  /// Altitude.
+  final double altitude;
+
+  LatLng toLatLng() {
+    return LatLng(latitude, longitude);
+  }
+
+  /// Linear interpolation of two [LatLng]s.
+  static LatLngAlt lerp(LatLngAlt a, LatLngAlt b, double t) {
+    final lat = l.lerp(a.latitude, b.latitude, t);
+    final lng = l.lerp(a.longitude, b.longitude, t);
+    final alt = l.lerp(a.altitude, b.altitude, t);
+
+    return LatLngAlt(lat, lng, alt);
+  }
+}
